0f17efcd5810ad963d49af50f3e85ce7bedf650f
jjr2930 <joojungyeol@gmail.com>
2022-06-03 22:43:47 +0900

    db 연결 구조 작업중...

---
+++ AccountGenerationServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using DBServer;
+
+
+namespace AccountGenerationServer
+{
+    class MainClass
+    { 
+        public static void Main(string[] args)
+        {
+            var ipinfos = IPInfoList.FromFile("/Users/jujeong-yeol/Documents/Educations/Database/SourceCode/ipList.txt");
+            DBClient[] clients = new DBClient[ipinfos.ips.Count];
+            for (int i = 0; i < ipinfos.ips.Count; i++)
+            {
+                string ip = ipinfos.ips[i].ip;
+                int port = ipinfos.ips[i].port;
+                DBClient newClient = new DBClient(ip, port);
+                clients[i] = newClient;
+            }
+
+            while(true)
+            {
+                Console.WriteLine("Type user id");
+                var id = Console.ReadLine();
+
+                Console.WriteLine("type your password");
+                var password = Console.ReadLine();
+
+                int serverCount = 4;
+                int serverIndex = -1;
+                using(var sha512 = new SHA512Managed())
+                {
+                    var hash = new List<byte>(sha512.ComputeHash(Encoding.UTF8.GetBytes(id)));
+                    hash.Add(0);
+                    BigInteger number = new BigInteger(hash.ToArray());
+                    BigInteger index = number % serverCount;
+                    serverIndex = (int)(index);
+                }
+
+                clients[serverIndex].AddUser(id);
+            }
+        }
+    }
+}
